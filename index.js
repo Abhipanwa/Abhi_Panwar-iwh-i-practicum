274aa205fa6a9a303343713de86be8ac835148b0
Abhipanwa <167404400+Abhipanwa@users.noreply.github.com>
2024-05-02 14:23:04 +0530

    index.js

--- index.js
+++ index.js
@@ -1,71 +1,128 @@
 const express = require('express');
 const axios = require('axios');
+const bodyParser = require('body-parser'); 
+const qs=require('qs')
+const path=require('path')
+
 const app = express();
-
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+//app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public'));
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const private_app_access = 'private_app_access_token';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get("/", async (req, res) => {
+    const contact = 'https://api.hubspot.com/crm/v3/objects/contacts?properties=email&properties=firstname&properties=lastname&properties=mobilephone&limit=100';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Authorization': `Bearer ${private_app_access}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        const response = await axios.get(contact, { headers });
+        const result = response.data.results;
+        console.log(result);
+        const data=result;
+        res.render("homepage", { data: data });
+    } catch (error) {
+        console.error("Getting an error", error);
+        res.status(500).send("Error in fetching data from the API");
     }
+});
+
+// Creating a new contact here 
+app.post("/update-cobj", async (req, res) => { 
+    const { email, firstname, lastname, phoneno } = req.body;
+    const axios = require('axios');
+                let data = JSON.stringify({
+                "properties": {
+                    "email": email,
+                    "firstname": firstname,
+                    "lastname": lastname,
+                    "mobilephone":phoneno
+                }
+                });
+
+                let config = {
+                method: 'post',
+                maxBodyLength: Infinity,
+                url: 'https://api.hubapi.com/crm/v3/objects/contacts',
+                headers: { 
+                    'authorization': `Bearer ${private_app_access}`, 
+                    'content-type': 'application/json'
+                },
+                data : data
+                };
+
+                axios.request(config)
+                .then((response) => {
+                console.log(JSON.stringify(response.data));
+                console.log("Data has been created successfully!\n",data);
+                res.redirect("/");
+                })
+                .catch((error) => {
+                console.log(error);
+                res.send("Getting an error to create new data record",error);
+                });
 
 });
-*/
 
+// Updating a contact data by ID
+app.get("/update-cobj/:id", async (req, res) => { 
+    const id = req.params.id;
+    const contactUpdateUrl = `https://api.hubspot.com/crm/v3/objects/contacts/${id}`;
+    const headers = {
+        Authorization: `Bearer ${private_app_access}`,
+        'Content-Type': 'application/json'
+    };
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+    const updatedValue = {
+        properties: {
+            firstname: "Vishal",
+            lastname: "kumar",
+            email: "vishalkumar8877@gmail.com",
+            mobilephone: "+999999999999"
+        }
+    };
+
+    try {
+        const response = await axios.patch(contactUpdateUrl, updatedValue, { headers });
+        const data = response.data;
+        console.log("Contact has been updated successfully!", data);
+        res.redirect("/");
+    } catch (error) {
+        console.error("Error updating contact:", error);
+        res.status(500).send("Error updating contact: " + error.message);
+    }
+});
+
+// Deleting a contact data by ID
+app.get("/delete/:id", async (req, res) => { 
+    const id = req.params.id;
+    const contact = `https://api.hubapi.com/crm/v3/objects/contacts/${id}`; 
+    const headers = {
+        Authorization: `Bearer ${private_app_access}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.delete(contact, { headers });
+        const data = response.data;
+        console.log("Data has been Deleted Successfully!");
+        res.redirect("/");
+    } catch (error) {
+        console.error("Getting an Error to delete the data ", error);
+        res.status(500).send("Getting an Error to delete the data " + error.message);
+    }
+});
+
+// Rendering the updates.pug here
+app.get("/update-form", (req, res) => {
+    res.render("updates"); 
+});
+
+app.listen(3000, () => {
+    console.log("Output is running on: localhost:3000");
+});
